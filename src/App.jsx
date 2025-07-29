8623e2f086d160c70b993eb5c1a610e1f1f92c2e
Dante Silva <dantesilvacodes@gmail.com>
2025-07-28 20:30:23 -0400

    SortSelector Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
 import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
+
 //API URL for the coins data.
 const API_URL = import.meta.env.VITE_API_URL;
 // "&order=market_cap_desc&per_page=10&page=1&sparkline=false"
@@ -17,6 +19,8 @@ const App = () => {
   const [limit, setLimit] = useState(10);
   //State variable to store the filter value.
   const [filter, setFilter] = useState("");
+  //State variable to store the sort by value.
+  const [sortBy, setSortBy] = useState("market_cap_desc");
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -26,7 +30,7 @@ const App = () => {
         );
         if (!response.ok) throw new Error("Network response was not ok");
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         //set the coins state to the data
         setCoins(data);
       } catch (error) {
@@ -38,12 +42,29 @@ const App = () => {
     fetchCoins();
   }, [limit]);
 
-  const filteredCoins = coins.filter((coin) => {
-    return (
-      coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-      coin.symbol.toLowerCase().includes(filter.toLowerCase())
-    );
-  });
+  const filteredCoins = coins
+    .filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLowerCase())
+      );
+    })
+    .slice(0, limit)
+    .sort((a, b) => {
+      switch (sortBy) {
+        case "market_cap_desc":
+          return b.market_cap - a.market_cap;
+        case "market_cap_asc":
+          return a.market_cap - b.market_cap;
+        case "price_desc":
+          return b.current_price - a.current_price;
+        case "price_asc":
+          return a.current_price - b.current_price;
+        case "change_desc":
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+        case "change_asc":
+      }
+    });
 
   return (
     <div>
@@ -53,6 +74,7 @@ const App = () => {
       <div className="top-controls">
         <FilterInput filter={filter} onFilterChange={setFilter} />
         <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
       </div>
       {!loading && !error && (
         <main className="grid">
